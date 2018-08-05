ca265074650d0cc75e86496497707dbae8b6c2f7
Marc Hoeve <mrhoeve@gmail.com>
2018-08-05 18:21:12 +0200

    Developed base idea
    - Plugin system working
    - Base string localisation

---
+++ Korfbal/KorfbalExtensionInformation.cs
@@ -0,0 +1,66 @@
+﻿using myTournamentExtensionInterfaces.SportExtension;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Korfbal
+{
+    class KorfbalExtensionInformation : ISportExtensionInformation
+    {
+        public string extensionName
+        {
+            get
+            {
+                return Localisation.strings.extensionName;
+            }
+        }
+
+        public Version extensionVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version;
+            }
+        }
+
+        public string extensionDevelopers
+        {
+            get
+            {
+                // Get the developers from the Contributors file and return it
+                return readEmbeddedFile("CONTRIBUTORS");
+            }
+        }
+
+        public string appropriateLicenseShort
+        {
+            get
+            {
+                return "MIT License";
+            }
+        }
+
+        public string appropriateLicenseLong
+        {
+            get
+            {
+                // Get the developers from the Contributors file and return it
+                return readEmbeddedFile("LICENSE");
+            }
+        }
+
+        private string readEmbeddedFile(string filename)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Korfbal." + filename;
+            string result;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+            return result;
+        }
+    }
+}
---
+++ myTournament/Helpers/LoadPlugins.cs
@@ -0,0 +1,24 @@
+﻿using myTournamentExtensionInterfaces.SportExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myTournament.Helpers
+{
+    class LoadPlugins
+    {
+        public LoadPlugins()
+        {
+            ICollection<ISportExtensionInformation> _sportExtensionInformation = GenericPluginLoader<ISportExtensionInformation>.LoadPlugins("Plugins");
+            if (_sportExtensionInformation != null)
+            {
+                foreach (var item in _sportExtensionInformation)
+                {
+                    Console.WriteLine($"Plugin loaded!\n\n{Localisation.strings.extensionName}: {item.extensionName}\n{Localisation.strings.extensionVersion}: {item.extensionVersion.ToString()}\n{Localisation.strings.extensionDeveloper}: {item.extensionDevelopers}\n{Localisation.strings.extensionAppropriateLicenseShort}: {item.appropriateLicenseShort}\n\n{item.appropriateLicenseLong}");
+                }
+            }
+        }
+    }
+}
